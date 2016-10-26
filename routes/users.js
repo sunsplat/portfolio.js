9b98aeb4deb73b120db7ad6e375e3a4db7e13cb6
Ellen <heeycutie@yahoo.com>
2016-10-26 20:10:06 +0000

    add user capability

--- routes/users.js
+++ routes/users.js
@@ -12,4 +12,17 @@ router.get('/userlist', function(req, res) {
     });
 });
 
+/*
+ * POST to adduser.
+ */
+router.post('/adduser', function(req, res) {
+    var db = req.db;
+    var collection = db.get('userlist');
+    collection.insert(req.body, function(err, result){
+        res.send(
+            (err === null) ? { msg: '' } : { msg: err }
+        );
+    });
+});
+
 module.exports = router;

db62e7109221f2e016db328e0c5d04081d48ceac
Ellen <heeycutie@yahoo.com>
2016-10-27 18:13:12 +0000

    adding about page and styling for it

--- routes/index.js
+++ routes/index.js
@@ -3,19 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Ellen\'s Portfolio' });
+  res.render('index', { title: 'My Portfolio' });
 });
 
 router.get('/experience', function(req, res, next) {
-  res.render('experience', { title: 'Ellen\'s Resume' });
+  res.render('experience', { title: 'My Resume' });
 });
 
 router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'About Ellen' });
+  res.render('about', { title: 'About Me' });
 });
 
 router.get('/contact', function(req, res, next) {
-  res.render('contact', { title: 'Contact Ellen' });
+  res.render('contact', { title: 'Contact Me' });
 });
 
 module.exports = router;

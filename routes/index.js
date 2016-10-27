75c52a0a06e358911e2a88711dd1a8a47a93036c
Ellen <heeycutie@yahoo.com>
2016-10-27 17:25:36 +0000

    adding initial pages for contact, experience, portfolio, and about

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Ellen\'s Portfolio' });
 });
 
+router.get('/portfolio', function(req, res, next) {
+  res.render('portfolio', { title: 'Ellen\'s Portfolio' });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About Ellen' });
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact Ellen' });
+});
+
 module.exports = router;

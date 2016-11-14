de404a01b805eb9916464776be1a1976e3923b51
sunsplat <heeycutie@yahoo.com>
2016-11-14 18:11:30 -0500

    making page responsive

--- public/javascripts/masonry.js
+++ public/javascripts/masonry.js
@@ -2,12 +2,19 @@
 
 var grid = document.querySelector('.grid');
 var msnry;
-
+var columns = 3;
+var setColumns = function() {
+	columns = window.innerWidth > 640 ? 3 : window.innerWidth > 320 ? 2 : 1;
+	console.log(columns);
+};
+setColumns();
+window.onresize(setColumns);
 imagesLoaded( grid, function() {
   // init Isotope after all images have loaded
   msnry = new Masonry( grid, {
     itemSelector: '.grid-item',
-    columnWidth: '.grid-sizer',
+    //columnWidth: '.grid-sizer',
+    columnWidth: function(containerWidth) { return containerWidth/columns; },
     percentPosition: true,
     gutter: 10
   });

06b31ba3acb768c05c09fd90755a6e23bf86a6fb
sunsplat <heeycutie@yahoo.com>
2016-11-16 17:05:30 -0500

    fixed masonry

--- public/javascripts/masonry.js
+++ public/javascripts/masonry.js
@@ -2,20 +2,12 @@
 
 var grid = document.querySelector('.grid');
 var msnry;
-var columns = 3;
-var setColumns = function() {
-	columns = window.outerWidth > 640 ? 3 : window.outerWidth > 320 ? 2 : 1;
-	console.log(columns);
-};
-setColumns();
-window.onresize(setColumns);
+
 imagesLoaded( grid, function() {
   // init Isotope after all images have loaded
   msnry = new Masonry( grid, {
     itemSelector: '.grid-item',
-    //columnWidth: '.grid-sizer',
-    columnWidth: function(containerWidth) { return containerWidth/columns; },
-    percentPosition: true,
+    columnWidth: '.grid-sizer',
     gutter: 10
   });
 });
